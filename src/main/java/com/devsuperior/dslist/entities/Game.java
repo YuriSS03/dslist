802eb0c459f40d8cfefe53712ac6bbcdb294ec32
Yuri Soares <yuridesouzasilva16@gmail.com>
2025-01-22 16:38:11 -0300

    Game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -18,21 +18,27 @@ public class Game {
 
    @Column(name = "game_year")
    private Integer year;
+   private String genre;
    private String platforms;
    private Double score;
    private String imgUrl;
+   
+   @Column(columnDefinition = "TEXT")
    private String shortDescription;
+
+   @Column(columnDefinition = "TEXT")
    private String longDescription;
 
     public Game(){
 
     }
 
-    public Game(Long id, String title, Integer year, String platforms, Double score, String imgUrl,
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
             String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
+        this.genre = genre;
         this.platforms = platforms;
         this.score = score;
         this.imgUrl = imgUrl;
@@ -62,6 +68,14 @@ public class Game {
 
     public void setYear(Integer year) {
         this.year = year;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
     }
 
     public String getPlatforms() {
